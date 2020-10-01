17a40ae9a4d8e4518ae171799a103c7b19d74214
William Boyle <billy.boyle@mail.utoronto.ca>
2020-10-01 13:04:25 -0400

    Chapter 4 examples added
    - Copied examples 4-1 to 4-7
    - Added email field to the form
    - Added functionality to handle invalid email inputs

--- hello.py
+++ hello.py
@@ -1,15 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email, Regexp
 from datetime import datetime
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        form.name.data = ''
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['email'] = form.email.data
+        form.email.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
@@ -22,6 +41,11 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField("What is your UofT Email address", validators=[Email()])
+    submit = SubmitField('Submit')
 
 if __name__ == '__main__':
     app.run(debug=True)
